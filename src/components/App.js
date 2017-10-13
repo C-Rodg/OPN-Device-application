50d4804a55f5de6c62aaaedfb9ec88bcd5aaf807
C-Rodg <rodgersbc@gmail.com>
2017-10-13 10:01:03 -0700

    Setup nav routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,25 @@
 import React, { Component } from "react";
 import "../styles/default.css";
-import { Route } from 'react-router-dom';
+import { Route, NavLink } from 'react-router-dom';
 
 // Electron Communication
 const electron = require("electron");
 let { remote } = electron;
 const { ipcRenderer } = electron;
 
-const TestRoute = () => {
+const SummaryRoute = () => {
 	return (
-		<div className="TEST">TEST ROUTE SHOWED!</div>
+		<div className="TEST">Summary!</div>
+	);
+};
+const DataRoute = () => {
+	return (
+		<div className="TEST">Dataaa!</div>
+	);
+};
+const DevicesRoute = () => {
+	return (
+		<div className="TEST">Devicesss!</div>
 	);
 };
 
@@ -37,9 +47,20 @@ class App extends Component {
 	render() {
 		return (
 			<div className="app">
-				My App content here...
+				<nav>
+					<div className="nav-title">
+						Validar Quickscan Central
+					</div>
+					<div className="nav-items">
+						<NavLink to="/" activeClassName="nav-active">Summary</NavLink>
+						<NavLink to="/data" activeClassName="nav-active">Data</NavLink>
+						<NavLink to="/devices" activeClassName="nav-active">Devices</NavLink>
+					</div>
+				</nav>
 				<main>
-					<Route path="/test" exact component={TestRoute} />
+					<Route path="/" exact component={SummaryRoute} />
+					<Route path="/data" exact component={DataRoute} />
+					<Route path="/devices" exact component={DevicesRoute} />
 				</main>
 			</div>
 		);
